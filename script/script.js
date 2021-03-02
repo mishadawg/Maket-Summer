eea6ea47f4aac7e6f6faa37d43fe0828fcb3148c
Misha <misha40401@yandex.ru>
2021-03-02 21:28:16 +0300

    menu done

--- script/script.js
+++ script/script.js
@@ -1,17 +1,67 @@
 // Menu
 const menuList = document.getElementsByClassName('containerMenu');
+const home = document.getElementById('home');
+const etcPage = document.getElementById('etc');
+
 for (let i = 0; i < menuList.length; i++) {
     menuList[i].addEventListener('click',menu,false);
 }
-//ToDo обратиться затем к боди и найти секции необходимы и при переключении выдавать нужную секцию
-// Сделать Модальные окна
-// Выпадающие тексты
-// Карусель внизу
 function menu(e){
     let referal = e.target.getAttribute('href');
+    let checkHome = home.classList.contains('hide');
+    let checkEtc = home.classList.contains('hide');
+    
     if(referal == "#home"){
-        console.log('HOME');
-    }else{
-        console.log(referal);
+        addClassMenu(etcPage)
+        if(checkHome == true){
+            deleteClassMenu(home);
+        }
+        else{
+            deleteClassMenu(home);
+        }
+    }else if(referal == '#'){
+        addClassMenu(home)
+        if(checkEtc == true){
+            deleteClassMenu(etcPage);
+        }
+        else{
+            deleteClassMenu(etcPage);
+        }
     }
 }
+function deleteClassMenu(content) {
+    content.classList.remove('hide');  
+}
+function addClassMenu(content) {
+    content.classList.add('hide');  
+}
+// End Menu
+
+// TODO Выпадающие тексты
+// Text Opens
+
+// End Text Opens
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO Сделать Модальные окна
+// Modal windows
+
+// End Modal windows
+
+// TODO Карусель внизу
+// Carusel
+
+// End Carusel
+
